d975b61a5cdbaa737fcb3107b475a1e17ff0d979
Alex Acevedo <alex.acpe@outlook.com>
2022-05-15 22:39:06 -0500

    Creacion de estructuras de carpetas y creacion de clases js, py, java

---
+++ Java/Car.java
@@ -0,0 +1,8 @@
+package Java;
+//Inicialmente driver es de tipo string pero eventualmente será de tipo driver
+public class Car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passenger;
+}
---
+++ Python/route.py
@@ -0,0 +1,8 @@
+from sys import int_info
+from tracemalloc import start
+
+
+class Route:
+    id      = int
+    start   = []
+    end     = []
